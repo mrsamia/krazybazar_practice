a4884379087c4af8891ee9c0f82272a4be80b27d
mrsamia <15203060@iubat.edu>
2022-12-18 17:57:22 +0600

    SRM |concate

--- src/Practice/TodoTwo.js
+++ src/Practice/TodoTwo.js
@@ -6,18 +6,26 @@ function TodoTwo(props) {
   //   const[delte, setDelte] = useState()
 
   function clickHandler() {
-    setList([...list, input]);
+    setList([...list, input+"-A"]);
     setInput("");
   }
+
   function changeHandler(e) {
-    setInput(e.target.value);
+    setInput(e.target.value );
   }
 
-  const removeTask = (index) => {
-    const newTasks = [...list];
-    newTasks.splice(index, 1);
-    setList(newTasks);
-  };
+//   const removeTask = (index) => {
+//     const newTasks = [...list];
+//     newTasks.splice(index, 1);
+//     setList(newTasks);
+//   };
+
+//not working
+//  function removeTask(index){
+//     const deleteTask=[...list];
+//     deleteTask.slice(index,1)
+//     setList(deleteTask);
+// }
 
   return (
     <div className="pb-5">
@@ -28,7 +36,9 @@ function TodoTwo(props) {
           {list.map((item) => (
             <div className="container d-flex">
               <li>{item}</li>
-              <button onClick={() => removeTask()}>delete</button>
+              {/* <button onClick={() => removeTask()}>delete</button> */}
+              {/* <button onClick={removeTask}>delete</button> */}
+              <button>delete</button>
             </div>
           ))}
         </h3>
