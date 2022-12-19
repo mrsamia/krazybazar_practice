a23274afdcbd27a0aa05fac6a0b794bcc380eed4
mrsamia <15203060@iubat.edu>
2022-12-19 17:30:53 +0600

    SRM | emailform

--- src/Practice/EPform.js
+++ src/Practice/EPform.js
@@ -5,6 +5,9 @@ function EPform(props) {
   const [password, setPassword] = useState();
   const [confirmpas, setConfirmpas] = useState();
   const[show ,setShow]=useState(false)
+  const[active ,setActive]=useState(false)
+
+
   function emailHandler(event) {
     setEmail(event.target.value);
   }
@@ -26,16 +29,23 @@ function clickHandler(){
       </div>
       <div>
         <h6>Password :</h6>
-        <input type={show ?"password":"text"} placeholder="Password" onChange={passHandler} onClick={clickHandler}/>
+        <input type={show ?"password":"text"} placeholder="Password" value={password} onChange={passHandler} onClick={clickHandler}/>
       </div>
       <div>
         <h6>Confirm Password :</h6>
         <input
           type="password"
+          value={confirmpas}
           placeholder="Confirm Password"
           onChange={confirmPasHandler}  
         />
       </div>
+      {/* <div>
+      { password===confirmpas? <button >Submit</button>:
+        <button disabled>Submit</button>}
+      </div> */}
+      <button disabled={password===confirmpas?false:true}>Submit</button>
+    
     </div>
   );
 }
